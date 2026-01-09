a7815456f64a79a6605b6ce86578387ea0fc5c90
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2026-01-09 15:52:29 +0200

    Created form for create new person

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
 
 from core.models import Person
@@ -9,4 +11,20 @@ class PersonListView(generic.ListView):
 
 
 def person_create_view(request):
-    return render(request, "core/person_list.html")
+    if request.method == "GET":
+        return render(request, "core/person_form.html")
+    elif request.method == "POST":
+        full_name = request.POST.get("full_name")
+        birth_year = request.POST.get("birth_year")
+        if full_name and birth_year:
+            Person.objects.create(
+                full_name=full_name,
+                birth_year=birth_year, )
+            return HttpResponseRedirect(reverse("core:person-list"))
+
+        else:
+            context = {
+                "error": "Please, provide all information"
+            }
+            return render(request, "core/person_form.html", context=context)
+
